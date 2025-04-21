fb0a4375863db3d5442cf5960f8dc8fd69e3c4ff
brayway05 <brayway05@gmail.com>
2025-04-21 14:17:42 -0600

    Missing s3 bucket

--- dev_terraform/modules/s3/main.tf
+++ dev_terraform/modules/s3/main.tf
@@ -6,3 +6,7 @@ resource "aws_s3_bucket" "star_command_video_storage" {
   bucket = "star-command-video-storage"
   force_destroy = true
 }
+
+resource "aws_s3_bucket" "model_artifacts" {
+  bucket = "model-artifacts-spot"
+}
