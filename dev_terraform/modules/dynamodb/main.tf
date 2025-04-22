182fb620fc8151ec84d8384f984b9916bda09663
Ben Cullen <ben.cullen.n@gmail.com>
2025-04-21 19:12:54 -0600

    Added dynamoDB table for kirk event manager

--- dev_terraform/modules/dynamodb/main.tf
+++ dev_terraform/modules/dynamodb/main.tf
@@ -19,3 +19,36 @@ resource "aws_dynamodb_table" "inferencing_predictions" {
     projection_type = "ALL"
   }
 }
+
+resource "aws_dynamodb_table" "events" {
+  name         = "events"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "camera"
+  range_key    = "timestamp"
+
+  attribute {
+    name = "camera"
+    type = "S"
+  }
+
+  attribute {
+    name = "timestamp"
+    type = "S"
+  }
+
+  attribute {
+    name = "date"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "date-index"
+    hash_key        = "date"
+    range_key       = "timestamp"
+    projection_type = "ALL"
+  }
+
+  tags = {
+    Name = "Events Table"
+  }
+}
